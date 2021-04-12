ef2fb392da7c2e12c3dc91937244e9af0c2d24ea
YuliaOv22 <ovvera12@mail.ru>
2021-04-12 20:17:02 +0300

    GB parser

--- database/database.py
+++ database/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from . import models
 
 
@@ -9,25 +10,74 @@ class Database:
         models.Base.metadata.create_all(bind=engine)
         self.maker = sessionmaker(bind=engine)
 
-    def _get_or_create(self, session, model, unique_field, data: dict):
-        instance: model = session.query(model).filter_by(
-            **{unique_field: data[unique_field]}
-        ).first()
-        if not instance:
-            instance = model(**data)
-        return instance
+    def _get_or_create(self, session, model, uniq_field, uniq_value, **data):
+        db_data = session.query(model).filter(uniq_field == uniq_value).first()
+        if not db_data:
+            db_data = model(**data)
+            session.add(db_data)
+            try:
+                session.commit()
+            except Exception as exc:
+                print(exc)
+                session.rollback()
+        return db_data
+
+    def _get_or_create_comments(self, session, data: list) -> list:
+        result = []
+        if data:
+            for comment in data:
+                comment_author = self._get_or_create(
+                    session,
+                    models.Author,
+                    models.Author.url,
+                    comment["comment"]["user"]["url"],
+                    name=comment["comment"]["user"]["full_name"],
+                    url=comment["comment"]["user"]["url"],
+                )
+                db_comment = self._get_or_create(
+                    session,
+                    models.Comment,
+                    models.Comment.id,
+                    comment["comment"]["id"],
+                    **comment["comment"],
+                    author=comment_author,
+                )
+
+                result.append(db_comment)
+                result.extend(
+                    self._get_or_create_comments(session, comment["comment"]["children"])
+                )
+
+        return result
 
     def create_post(self, data):
         session = self.maker()
-        post = self._get_or_create(session, models.Post, "url", data["post_data"])
-        author = self._get_or_create(session, models.Author, "url", data["author_data"])
-        tags = [
-            self._get_or_create(session, models.Tag, "url", tag_data)
-            for tag_data in data["tags_data"]
-        ]
-        post.author = author
+        comments = self._get_or_create_comments(session, data["comments_data"])
+        author = self._get_or_create(
+            session,
+            models.Author,
+            models.Author.url,
+            data["author_data"]["url"],
+            **data["author_data"],
+        )
+        tags = map(
+            lambda tag_data: self._get_or_create(
+                session, models.Tag, models.Tag.url, tag_data["url"], **tag_data
+            ),
+            data["tags_data"],
+        )
+        post = self._get_or_create(
+            session,
+            models.Post,
+            models.Post.id,
+            data["post_data"]["id"],
+            **data["post_data"],
+            author=author,
+        )
         post.tags.extend(tags)
+        post.comments.extend(comments)
         session.add(post)
+
         try:
             session.commit()
         except Exception as exc:
@@ -35,3 +85,4 @@ class Database:
             session.rollback()
         finally:
             session.close()
+        print(1)
