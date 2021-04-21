79d4c38404602d80150b419302c0cf385368c48e
YuliaOv22 <ovvera12@mail.ru>
2021-04-22 00:38:12 +0300

    avito_parser created

--- avito_parse/spiders/xpaths.py
+++
@@ -1,5 +0,0 @@
-AUTO_YOULA_PAGE_XPATH = {
-    "brands": "//div[@data-target='transport-main-filters']/"
-    "div[contains(@class, 'TransportMainFilters_brandsList')]//"
-    "a[@data-target='brand']/@href",
-}
