93f71d0fdc18af59dc59bb904371d62b4c7adf8a
YuliaOv22 <ovvera12@mail.ru>
2021-04-26 22:59:01 +0300

    Created final project Instagram

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import dotenv
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from instagram_parse.spiders.instagram import InstagramSpider
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv(".env", override=True)
+    crawler_settings = Settings()
+    crawler_settings.setmodule("instagram_parse.settings")
+    crawler_proc = CrawlerProcess(settings=crawler_settings)
+    users = ["juliaju_o", "andrew_churkin"]
+    crawler_proc.crawl(
+        InstagramSpider,
+        login=os.getenv("USERNAME"),
+        password=os.getenv("ENC_PASSWORD"),
+        users=users
+    )
+    crawler_proc.start()
